7214f679040fed324457b981cc9550079d064202
Logan Smith <logsmith06@gmail.com>
2019-09-28 21:41:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,5 +138,5 @@ def adding_to_dennis
           :languages => ["C", "Assembly"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] 
+programmer_hash[:dennis_ritchie][:languages]
 end
