fa55b68d204511ab77c63e495903139a80408c82
Logan Smith <logsmith06@gmail.com>
2019-09-28 21:12:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages]
+		 dennis_ritchies_language = programmer_hash[:dennis_ritchie][languages]
 		 p dennis_ritchies_language
 
 end
