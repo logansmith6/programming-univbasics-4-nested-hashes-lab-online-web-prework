a2a7789016e8aaec9754fb4ec873ca7d4d9fd1f1
Logan Smith <logsmith06@gmail.com>
2019-09-28 21:33:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,9 +138,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash = {
-	:dennis_ritchie => {
-		:known_for => '"GUI", "Assemly"'
-	}
-}
+programmer_hash[:dennis_ritchie][languages][1] = "Assembly"
 end
